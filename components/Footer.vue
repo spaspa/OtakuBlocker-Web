12ad649dc8e21a4ee2bf4f8dd09bdd950785aced
Eleven-Lines <yoyamssk@gmail.com>
2018-07-17 23:09:31 +0900

    Add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,4 +1,20 @@
-<template>
-  <footer>
-  </footer>
+<template lang="pug">
+  footer
+    hr
+    | Made with
+    span.icon.is-small
+      i.fas.fa-heart
+    | by 
+    a(href="https://eleven-lines.github.io/")
+      | Spa
 </template>
+
+<style lang="sass" scoped>
+@import "~/assets/css/main.sass"
+footer
+  text-align: center
+  margin-bottom: 2rem
+span
+  margin: 0px 0.5rem
+  color: $primary
+</style>
