f085814f60e805769ec5b282669e68cbe1e8b5e0
Eleven-Lines <yoyamssk@gmail.com>
2018-07-20 14:37:25 +0900

    add some utility API

--- api/routes/twitter.js
+++ api/routes/twitter.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const querystring = require('querystring')
 
 const router = Router()
 
@@ -11,11 +12,115 @@ const client = req => new Twitter({
   access_token_secret: req.session.oauth.access_token_secret
 })
 
-router.get('/twitter/*', function (req, res, next) {
+// An API wrapper for cursoring.
+// Parameter "key" represents attribute for main content array in response. 
+router.get('/twitter/util/concat_cursor/*', async (req, res, next) => {
+  if (req.session && !req.session.oauth) {
+    res.status(401).send('unauthorized')
+  }
+  req.query.cursor = -1
+  const key = req.query.key ? req.query.key : 'users'
+
+  const tmpClient = client(req)
+  let data = []
+  while (req.query.cursor !== 0) {
+    let response = {}
+    try {
+      response = await tmpClient.get(req.params['0'], req.query)
+      data = data.concat(response[key])
+      req.query.cursor = response.next_cursor
+    }
+    catch (err) {
+      console.log(err)
+      break
+    }
+  }
+  res.json(data)
+})
+
+// An API wrapper for concatenating by max_id.
+// Assuming that response is an array. 
+// Parameter "max_count" is passed to API as "count" parameter.
+// Usage: Get 1000 tweets from @twitter by collecting 100 tweets per each API call.
+//   GET /twitter/util/concat_id/statuses/user_timeline?screen_name=twtiter&count=1000&max_count=100
+router.get('/twitter/util/concat_id/*', async (req, res, next) => {
+  if (req.session && !req.session.oauth) {
+    res.status(401).send('unauthorized')
+  }
+  const maxCount = req.query.max_count ? req.query.max_count : 200
+  const totalCount = req.query.count ? req.query.count : maxCount
+  delete req.query.max_count
+  req.query.count = maxCount
+
+  const tmpClient = client(req)
+  let data = []
+  let maxId = -1
+  while (data.length < totalCount) {
+    if (maxId !== -1) {
+      req.query.max_id = maxId
+    }
+    let response = []
+    try {
+      response = await tmpClient.get(req.params['0'], req.query)
+    }
+    catch (err) {
+      res.json(err)
+    }
+    data = data.concat(response)
+    if (!response[response.length - 1]) {
+      break
+    }
+    maxId = response[response.length - 1].id
+  }
+  res.json(data)
+})
+
+router.get('/twitter/util/search_tweets', async (req, res, next) => {
+  if (req.session && !req.session.oauth) {
+    res.status(401).send('unauthorized')
+  }
+  const maxCount = 100
+  const totalCount = req.query.count ? req.query.count : maxCount
+  req.query.count = maxCount
+
+  const tmpClient = client(req)
+  let data = []
+  let maxId = 0
+  while (data.length < totalCount) {
+    req.query.max_id = maxId
+    let response = {}
+    try {
+      response = await tmpClient.get('/search/tweets', req.query)
+    }
+    catch (err) {
+      res.json(err)
+    }
+    const next_results = response.search_metadata.next_results
+    data = data.concat(response.statuses)
+    maxId = querystring.parse(next_results).max_id
+  }
+  res.json(data)
+})
+
+router.get('/twitter/*', (req, res, next) => {
   if (req.session && !req.session.oauth) {
     res.status(401).send('unauthorized')
   }
   client(req).get(req.params['0'], req.query)
+    .then(response => {
+      res.json(response)
+    })
+    .catch(err => {
+      console.log(err)
+      next(err)
+    })
+})
+
+router.post('/twitter/*', (req, res, next) => {
+  if (req.session && !req.session.oauth) {
+    res.status(401).send('unauthorized')
+  }
+  client(req).post(req.params['0'], req.body)
     .then(response => {
       res.json(response)
     })
