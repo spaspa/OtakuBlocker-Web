1a5f7786d945fd75f73c048cd8b25c45e365e9a8
Eleven-Lines <yoyamssk@gmail.com>
2018-07-23 11:12:02 +0900

    change some settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Otaku Blocker',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Get rid of annyoing Otaku.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
